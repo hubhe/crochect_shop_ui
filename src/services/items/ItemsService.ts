e68efb4f35b9e284b412a1dac96397db3e4bb3a9
hubhe <lapidusy@gmail.com>
2024-03-18 21:35:01 +0200

    create item card

--- src/services/items/ItemsService.ts
+++ src/services/items/ItemsService.ts
@@ -29,3 +29,12 @@ export async function updateItem(data: FormData): Promise<boolean> {
       return handleAuthError(error);
     }
   }
+
+  export async function getItems(): Promise<any> {
+    try {
+      const response = await userFetch.get<RegisterRensponse>('/item');
+      return response;
+    } catch (error) {
+      return handleAuthError(error);
+    }
+  }
